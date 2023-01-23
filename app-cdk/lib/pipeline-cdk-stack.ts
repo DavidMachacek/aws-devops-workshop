a59308f87a8ba565dd2c529688b2729327861def
David Machacek <david@davids-mbp-2.home>
2023-01-23 19:58:54 +0100

    Commit

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,17 +1,21 @@
-import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
-import * as codecommit from "aws-cdk-lib/aws-codecommit";
-import * as codepipeline from "aws-cdk-lib/aws-codepipeline";
-import * as codebuild from "aws-cdk-lib/aws-codebuild";
-import * as codepipeline_actions from "aws-cdk-lib/aws-codepipeline-actions";
-import * as ecr from "aws-cdk-lib/aws-ecr";
-import * as iam from "aws-cdk-lib/aws-iam";
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline'
+import * as codebuild from 'aws-cdk-lib/aws-codebuild'
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions'
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
 
 interface ConsumerProps extends StackProps {
     ecrRepository: ecr.Repository;
+    testAppFargateService: ecsPatterns.ApplicationLoadBalancedFargateService;
+    prodAppFargateService: ecsPatterns.ApplicationLoadBalancedFargateService;
 }
 
 export class PipelineCdkStack extends Stack {
+
     constructor(scope: Construct, id: string, props: ConsumerProps) {
         super(scope, id, props);
 
@@ -20,47 +24,34 @@ export class PipelineCdkStack extends Stack {
             description: "Repository for my application code and infrastructure",
         });
 
-        const pipeline = new codepipeline.Pipeline(this, "CICD_Pipeline", {
-            pipelineName: "CICD_Pipeline",
-            crossAccountKeys: false,
+        const unitTestProject = new codebuild.PipelineProject(this, 'UnitTestProject', {
+            environment: {
+                buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+                privileged: true,
+                computeType: codebuild.ComputeType.LARGE
+            },
+            buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml')
         });
 
-        const codeQualityBuild = new codebuild.PipelineProject(
-            this,
-            "Code Quality",
-            {
-                environment: {
-                    buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
-                    privileged: true,
-                    computeType: codebuild.ComputeType.LARGE
-                },
-                buildSpec: codebuild.BuildSpec.fromSourceFilename("buildspec_test.yml"),
-            }
-        );
 
-        const dockerBuildProject = new codebuild.PipelineProject(
-            this,
-            "DockerBuildProject",
-            {
-                environmentVariables: {
-                    IMAGE_TAG: { value: "latest" },
-                    IMAGE_REPO_URI: { value: props.ecrRepository.repositoryUri },
-                    AWS_DEFAULT_REGION: { value: process.env.CDK_DEFAULT_REGION },
-                },
-                environment: {
-                    buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
-                    privileged: true,
-                    computeType: codebuild.ComputeType.LARGE
-                },
-                buildSpec: codebuild.BuildSpec.fromSourceFilename(
-                    "buildspec_docker.yml"
-                ),
-            }
-        );
+        const dockerBuildProject = new codebuild.PipelineProject(this, 'DockerBuildProject', {
+            environmentVariables: {
+                'IMAGE_TAG': { value: 'latest' },
+                'IMAGE_REPO_URI': {value: props.ecrRepository.repositoryUri },
+                'AWS_ACCOUNT_ID': {value: process.env.CDK_DEFAULT_ACCOUNT },
+                'AWS_DEFAULT_REGION': {value: process.env.CDK_DEFAULT_REGION },
+            },
+            environment: {
+                buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+                privileged: true,
+                computeType: codebuild.ComputeType.LARGE
+            },
+            buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_docker.yml'),
+        });
 
-        const dockerBuildRolePolicy = new iam.PolicyStatement({
+        const dockerBuildRolePolicy =  new iam.PolicyStatement({
             effect: iam.Effect.ALLOW,
-            resources: ["*"],
+            resources: ['*'],
             actions: [
                 "ecr:GetAuthorizationToken",
                 "ecr:BatchCheckLayerAvailability",
@@ -73,34 +64,38 @@ export class PipelineCdkStack extends Stack {
                 "ecr:InitiateLayerUpload",
                 "ecr:UploadLayerPart",
                 "ecr:CompleteLayerUpload",
-                "ecr:PutImage",
-            ],
+                "ecr:PutImage"
+            ]
         });
-
         dockerBuildProject.addToRolePolicy(dockerBuildRolePolicy);
+
+        const pipeline = new codepipeline.Pipeline(this, 'CICD_Workshop_Pipeline', {
+            pipelineName: 'CICD_Workshop_Pipeline',
+            crossAccountKeys: false,
+        });
 
         const sourceOutput = new codepipeline.Artifact();
         const unitTestOutput = new codepipeline.Artifact();
         const dockerBuildOutput = new codepipeline.Artifact();
 
         pipeline.addStage({
-            stageName: "Source",
+            stageName: 'Source',
             actions: [
                 new codepipeline_actions.CodeCommitSourceAction({
-                    actionName: "CodeCommit",
+                    actionName: 'CodeCommit',
                     repository: sourceRepo,
                     output: sourceOutput,
-                    branch: "master",
+                    branch: "main",
                 }),
             ],
         });
 
         pipeline.addStage({
-            stageName: "Code-Quality-Testing",
+            stageName: 'Code-Quality-Testing',
             actions: [
                 new codepipeline_actions.CodeBuildAction({
-                    actionName: "Unit-Test",
-                    project: codeQualityBuild,
+                    actionName: 'Unit-Test',
+                    project: unitTestProject,
                     input: sourceOutput,
                     outputs: [unitTestOutput],
                 }),
@@ -108,10 +103,10 @@ export class PipelineCdkStack extends Stack {
         });
 
         pipeline.addStage({
-            stageName: "Docker-Push-ECR",
+            stageName: 'ECS',
             actions: [
                 new codepipeline_actions.CodeBuildAction({
-                    actionName: "docker-build",
+                    actionName: 'docker-step',
                     project: dockerBuildProject,
                     input: sourceOutput,
                     outputs: [dockerBuildOutput],
@@ -119,8 +114,33 @@ export class PipelineCdkStack extends Stack {
             ],
         });
 
-        new CfnOutput(this, "CodeCommitRepositoryUrl", {
-            value: sourceRepo.repositoryCloneUrlHttp,
+        pipeline.addStage({
+            stageName: 'Deploy-Test',
+            actions: [
+                new codepipeline_actions.EcsDeployAction({
+                    actionName: 'deployECS',
+                    service: props.testAppFargateService.service,
+                    input: dockerBuildOutput
+                }),
+            ]
         });
+
+        pipeline.addStage({
+            stageName: 'Deploy-Production',
+            actions: [
+                new codepipeline_actions.ManualApprovalAction({
+                    actionName: 'Approve-Prod-Deploy',
+                    runOrder: 1
+                }),
+                new codepipeline_actions.EcsDeployAction({
+                    actionName: 'deployECS',
+                    service: props.prodAppFargateService.service,
+                    input: dockerBuildOutput,
+                    runOrder: 2
+                })
+            ]
+        });
+
+        new CfnOutput(this, 'CodeCommitRepositoryUrl', { value: sourceRepo.repositoryCloneUrlHttp });
     }
 }
